78f8be93169c759f8544c47fd5e13c1f499220ed
Andi Lisdiarto <80059599+andi-lisdiarto@users.noreply.github.com>
2021-03-20 18:03:51 +0700

    Add files via upload

---
+++ pertemuan_4/Pert4_perkalian.php
@@ -0,0 +1,23 @@
+<html> 
+    <head> 
+        <title>Tabel Perkalian</title> 
+    </head> 
+<body> 
+
+
+    <?php
+    
+    echo "Tabel Perkalian <br>";
+    echo "=============== <br>";
+
+    $a = 12;
+
+    for ($i =15; $i < 46; $i+=2) {
+
+        $hasil = $a*$i;
+        echo "$a * $i = $hasil <br>";
+    }
+
+    ?>
+</body> 
+</html> 
